7f32484844eed4cfee1dd9f62dbc7f2e26ef14f3
Veronazavr <verochka10@list.ru>
2020-09-24 18:58:29 +0300

    added jacoco

---
+++ src/main/java/ru/netology/domain/Condition.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Condition {
+        String name;
+        int maxTemperature;
+        int minTemperature;
+        int currentTemperature;
+        boolean on;
+
+}
---
+++ src/test/java/ru/netology/domain/ConditionTest.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionTest {
+
+    @Test
+    public void shouldCreate() {
+        Condition conditioner = new Condition();
+    }
+
+
+}
